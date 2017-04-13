c6492ba6c042ab31636b3f3f089e9b471808050c
Tamas Herman <hermantamas@gmail.com>
2017-04-13 11:35:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chrome headless test
+
+As per this [Hackernews](https://news.ycombinator.com/item?id=14101233) thread,
+Chrome 59 have full cross-platform headless support.
+
+Official description of the feature is here: https://www.chromestatus.com/features/5678767817097216
+and it links to this example code: https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+
+This repo contains that exmple code.
---
+++ chrome-remote-interface-test.js
@@ -0,0 +1,24 @@
+// chrome --headless --remote-debugging-port=9222 https://chromium.org
+const CDP = require('chrome-remote-interface');
+
+CDP((client) => {
+  // Extract used DevTools domains.
+  const {Page, Runtime} = client;
+
+  // Enable events on domains we are interested in.
+  Promise.all([
+    Page.enable()
+  ]).then(() => {
+    return Page.navigate({url: 'https://example.com'});
+  });
+
+  // Evaluate outerHTML after page has loaded.
+  Page.loadEventFired(() => {
+    Runtime.evaluate({expression: 'document.body.outerHTML'}).then((result) => {
+      console.log(result.result.value);
+      client.close();
+    });
+  });
+}).on('error', (err) => {
+  console.error('Cannot connect to browser:', err);
+});
